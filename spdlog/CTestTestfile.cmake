6b4acc9f970e54ecb612b2afe65f1430a4fd0d1d
sunashe <18079612186@163.com>
2019-03-01 22:15:16 +0800

    -

--- spdlog/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ashe/Desktop/github/test_log/spdlog
-# Build directory: /Users/ashe/Desktop/github/test_log/spdlog
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- spdlog/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ashe/Desktop/github/test_log/spdlog/tests
-# Build directory: /Users/ashe/Desktop/github/test_log/spdlog/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(spdlog-utests "/Users/ashe/Desktop/github/test_log/spdlog/tests/spdlog-utests")
